70b69606c1700c9de3e8bff875ba75f073da2d57
Oz Dagli <h.oguzhandagli@gmail.com>
2025-10-14 15:28:40 -0500

    Update streamlit_app.py
    Request lib added.

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,3 +39,8 @@ if ingredient_list:
         session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is ordered!', icon="✅")
+
+#new section to display smoothiefroot nutrition info
+import request
+smoothiefroot_response = request.get("https:my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
