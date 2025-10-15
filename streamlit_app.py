1f86d02249e234c86ccc4a37cb95339933384567
Oz Dagli <h.oguzhandagli@gmail.com>
2025-10-15 08:58:17 -0500

    Update streamlit_app.py
    ref

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ from snowflake.snowpark.functions import col
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
-  """Choose the fruits you want in your custom Smoothie.
-  """
+    """Choose the fruits you want in your custom Smoothie.
+    """
 )
 
 name_on_order = st.text_input("Name on Smoothie")
@@ -21,16 +21,17 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredient_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
-    , max_selections= 5)
+    , max_selections=5
+)
 
-if ingredient_list: 
+if ingredient_list:
     ingredients_string = ''
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
     sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """','""" +name_on_order+ """')"""
+values (""" + ingredients_string + """,""" + name_on_order + """)"""
     time_to_insert = st.button('Submit Order')
-    
-	if time_to_insert: 
+
+    if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
